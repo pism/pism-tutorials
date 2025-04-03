01944fb17062386ded0beaeccf487cf1b879fd1b
Andy Aschwanden <andy.aschwanden@gmail.com>
2025-04-02 23:20:38 -0700

    Work on second tutorial.

---
+++ pism_tutorials/domain.py
@@ -0,0 +1,216 @@
+# Copyright (C) 2023-25 Andy Aschwanden
+#
+# This file is part of pism-tutorials.
+#
+# PISM-TUTORIALS is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# PISM-TUTORIALS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PISM; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# pylint: disable=unused-import
+
+"""
+Module provides functions to handle domains.
+"""
+
+import numpy as np
+import rioxarray
+import xarray as xr
+
+
+def new_range(x: np.array, dx: float):
+    """
+    Compute the center and half-width of a domain that will contain all values in `x`.
+
+    The resulting half-width is an integer multiple of `dx`.
+
+    Parameters
+    ----------
+    x : np.array
+        A 1D array of coordinate values.
+    dx : float
+        The desired resolution for the new domain.
+
+    Returns
+    -------
+    center : float
+        The center of the new domain.
+    Lx : float
+        The half-width of the new domain.
+    N : int
+        The number of grid points in the new domain.
+
+    Notes
+    -----
+    The function assumes that `x` is sorted in ascending order and that the spacing
+    between consecutive elements in `x` is uniform.
+    """
+    x_min = np.min(x)
+    x_max = np.max(x)
+    dx_old = np.abs(x[1] - x[0])
+
+    # Note: add dx_old because the cell centered grid interpretation implies
+    # that the domain extends by 0.5*dx_old past x_min and x_max
+    width = dx_old + (x_max - x_min)
+    center = 0.5 * (x_min + x_max)
+
+    # compute the number of grid points
+    # (in a cell centered grid the numbers of points and spaces are the same)
+    N = np.ceil(width / dx)
+
+    # compute the new domain half-width
+    Lx = 0.5 * N * dx
+
+    return center, Lx, int(N)
+
+
+def get_bounds(
+    ds: xr.Dataset,
+    base_resolution: int = 150,
+    multipliers: list | np.ndarray = [1, 2, 3, 6, 8, 10, 12, 16, 20, 24, 30],
+):
+    """
+    Compute the x and y boundaries for a given dataset and set of grid resolutions.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        The input dataset containing the x and y coordinates.
+    base_resolution : int, optional
+        The base resolution in meters, by default 150.
+    multipliers : list | np.ndarray, optional
+        A list or array of multipliers to compute the set of grid resolutions, by default [1, 2, 3, 6, 8, 10, 12, 16, 20, 24, 30].
+
+    Returns
+    -------
+    tuple
+        A tuple containing the x boundaries and y boundaries as lists.
+
+    Examples
+    --------
+    >>> ds = xr.Dataset({'x': ('x', np.linspace(0, 1000, 11)), 'y': ('y', np.linspace(0, 2000, 21))})
+    >>> x_bnds, y_bnds = get_bounds(ds)
+    >>> print(x_bnds, y_bnds)
+    """
+    x = ds.variables["x"][:]
+    y = ds.variables["y"][:]
+
+    # set of grid resolutions, in meters
+    dx = base_resolution * np.array(multipliers)
+
+    # compute x_bnds for this set of resolutions
+    center, Lx, _ = new_range(x.values, np.lcm.reduce(dx))
+    x_bnds = [center - Lx, center + Lx]
+
+    # compute y_bnds for this set of resolutions
+    center, Ly, _ = new_range(y.values, np.lcm.reduce(dx))
+    y_bnds = np.minimum(center - Ly, center + Ly), np.maximum(center - Ly, center + Ly)
+    return x_bnds, y_bnds
+
+
+def create_domain(
+    x_bnds: list | np.ndarray,
+    y_bnds: list | np.ndarray,
+    x_dim: str = "x",
+    y_dim: str = "y",
+    crs: str = "EPSG:3413",
+) -> xr.Dataset:
+    """
+    Create an xarray.Dataset representing a domain with specified x and y boundaries.
+
+    Parameters
+    ----------
+    x_bnds : list | np.ndarray
+        A list or array containing the minimum and maximum x-coordinate boundaries.
+    y_bnds : list | np.ndarray
+        A list or array containing the minimum and maximum y-coordinate boundaries.
+    x_dim : str, optional
+        The name of the x dimension, by default "x".
+    y_dim : str, optional
+        The name of the y dimension, by default "y".
+    crs : str, optional
+        The coordinate reference system (CRS) for the domain, by default "EPSG:3413".
+
+    Returns
+    -------
+    xr.Dataset
+        An xarray.Dataset containing the domain information, including coordinates,
+        boundary data, and mapping attributes.
+
+    Notes
+    -----
+    The dataset includes:
+    - `x` and `y` coordinates with associated metadata.
+    - A `mapping` DataArray with polar stereographic projection attributes.
+    - A `domain` DataArray with a reference to the `mapping`.
+    - `x_bnds` and `y_bnds` DataArrays representing the boundaries of the domain.
+
+    Examples
+    --------
+    >>> x_bnds = [0, 1000]
+    >>> y_bnds = [0, 2000]
+    >>> ds = create_domain(x_bnds, y_bnds)
+    >>> print(ds)
+    """
+    x_bnds_dim = f"{x_dim}_bnds"
+    y_bnds_dim = f"{y_dim}_bnds"
+    coords = {
+        x_dim: (
+            [x_dim],
+            [0],
+            {
+                "units": "m",
+                "axis": x_dim.upper(),
+                "bounds": x_bnds_dim,
+                "standard_name": "projection_x_coordinate",
+                "long_name": f"{x_dim}-coordinate in projected coordinate system",
+            },
+        ),
+        y_dim: (
+            [y_dim],
+            [0],
+            {
+                "units": "m",
+                "axis": y_dim.upper(),
+                "bounds": y_bnds_dim,
+                "standard_name": "projection_y_coordinate",
+                "long_name": f"{y_dim}-coordinate in projected coordinate system",
+            },
+        ),
+    }
+    ds = xr.Dataset(
+        {
+            "domain": xr.DataArray(
+                data=0,
+                dims=[y_dim, x_dim],
+                coords={x_dim: coords[x_dim], y_dim: coords[y_dim]},
+                attrs={
+                    "dimensions": f"{x_dim} {y_dim}",
+                },
+            ),
+            x_bnds_dim: xr.DataArray(
+                data=[[x_bnds[0], x_bnds[1]]],
+                dims=[x_dim, "nv2"],
+                coords={x_dim: coords[x_dim]},
+                attrs={"_FillValue": False},
+            ),
+            y_bnds_dim: xr.DataArray(
+                data=[[y_bnds[0], y_bnds[1]]],
+                dims=[y_dim, "nv2"],
+                coords={y_dim: coords[y_dim]},
+                attrs={"_FillValue": False},
+            ),
+        },
+        attrs={"Conventions": "CF-1.8"},
+    ).rio.set_spatial_dims(x_dim=x_dim, y_dim=y_dim)
+    ds.rio.write_crs(crs, inplace=True)
+    return ds
